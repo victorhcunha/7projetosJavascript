059dfa443ad7e7616a236b5bc82d23106908c339
Victor <vhrc@cin.ufpe.br>
2021-07-25 12:42:16 -0300

    mensagem carregando

--- Projeto3 - Clima/script.js
+++ Projeto3 - Clima/script.js
@@ -2,4 +2,12 @@ document.querySelector(".busca").addEventListener("submit", (event) =>{
     event.preventDefault();
 
     let input = document.querySelector("#searchInput").nodeValue;
+
+    if (input !== ""){
+        showWarning("Carregando...")
+    }
 })
+
+function showWarning(msg){
+    document.querySelector(".aviso").innerHTML = msg
+}
