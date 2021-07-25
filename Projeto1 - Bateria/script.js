e2bf927a8314db29db9686b0a8a30ea322104456
Victor <vhrc@cin.ufpe.br>
2021-07-25 11:20:00 -0300

    fim projeto1

--- Projeto1 - Bateria/script.js
+++ Projeto1 - Bateria/script.js
@@ -2,6 +2,16 @@ document.body.addEventListener("keyup", (event)=>{
     playsound(event.code.toLowerCase() );
 })
 
+
+document.querySelector(".composer button").addEventListener("click", ()=>{
+    let song = document.querySelector("#input").value;
+
+    if(song !== ""){
+        let songArray = song.split("")
+        playComposition(songArray)
+    }
+})
+
 function playsound(sound) {
     let audioElement = document.querySelector(`#s_${sound}`);
     let keyElement = document.querySelector(`div[data-key="${sound}"]`);
@@ -16,5 +26,18 @@ function playsound(sound) {
         setTimeout(()=>{
             keyElement.classList.remove("active")
         }, 300)
+    }
+}
+
+function playComposition(songArray){
+    let wait = 0;
+
+    for(let songItem of songArray){
+
+        setTimeout(()=> {
+        playsound(`key${songItem}`);
+        }, wait);
+
+        wait += 250;
     }
 }
