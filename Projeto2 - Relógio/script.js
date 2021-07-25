a0bd4f4a4648150e24f5340dbf5fedbc02f1634e
Victor <vhrc@cin.ufpe.br>
2021-07-25 12:26:28 -0300

    correcao de rotacao

--- Projeto2 - Relógio/script.js
+++ Projeto2 - Relógio/script.js
@@ -16,9 +16,9 @@ function updateClock() {
     let mDeg = ((360/60) * minute) - 90;
     let hDeg = ((360/12) * hour) - 90;
 
-    sElement.style.transform = `rotate(${sDeg}deg)`
-    sElement.style.transform = `rotate(${mDeg}deg)`
-    sElement.style.transform = `rotate(${hDeg}deg)`
+    sElement.style.transform = `rotate(${sDeg}deg)`;
+    mElement.style.transform = `rotate(${mDeg}deg)`;
+    hElement.style.transform = `rotate(${hDeg}deg)`;
 
 }
 
