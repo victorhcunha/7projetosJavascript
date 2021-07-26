7200e1e8411b8613c5d7117956a701e63770d7af
Victor Hugo Cunha <vhrc@cin.ufpe.br>
2021-07-25 22:41:48 -0300

    ordem: preencher antes de exibir

--- Projeto3 - Clima/script.js
+++ Projeto3 - Clima/script.js
@@ -33,8 +33,6 @@ document.querySelector(".busca").addEventListener("submit", async(event) =>{
 function showInfo(json){
     showWarning("");
 
-    document.querySelector(".resultado").style.display = "block";
-
     document.querySelector(".titulo").innerHTML = `${json.name}, ${json.country}`;
     document.querySelector(".tempInfo").innerHTML = `${json.temp} <sup>Cº</sup>`;
     document.querySelector(".ventoInfo").innerHTML = `${json.windSpeed}`;
@@ -43,6 +41,7 @@ function showInfo(json){
     document.querySelector(".temp img").setAttribute("src", `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`);
     document.querySelector(".ventoPonto").style.transform = `rotate(${json.windAngle - 90}deg)`;
 
+    document.querySelector(".resultado").style.display = "block";
 }
 
 function clearInfo(){
