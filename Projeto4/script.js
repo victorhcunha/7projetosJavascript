f2294d86f6bb0e98f947434bb0dddc63ade5b72d
Victor <vhrc@cin.ufpe.br>
2021-07-26 07:31:42 -0300

    passando para ingles

--- Projeto4/script.js
+++ Projeto4/script.js
@@ -1,9 +1,13 @@
-// Dados iniciais
-let quadro = {
+// Inicial data
+let square = {
     a1: "", a2: "", a3:"",
     b1: "", b2: "", b3:"",
     c1: "", c2: "", c3:"",
-}
-// Eventos
+};
+let player = "";
+let warning = "";
+let playing = false;
 
-// Funções
+// Events
+
+// Functions
