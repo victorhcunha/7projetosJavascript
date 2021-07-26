8d608ec84ae1bbbdd0d1c1e65e18185cbe908977
Victor Hugo Cunha <vhrc@cin.ufpe.br>
2021-07-26 20:19:15 -0300

    resultados

--- Projeto6/script.js
+++ Projeto6/script.js
@@ -50,12 +50,23 @@ function optionsClickEvent(e){
 
 function finishQuiz(){
     let points = Math.floor((correctAnswers / questions.length) * 100);
+
+    if(points < 30){
+        document.querySelector(".scoreText1").innerHTML = "Tá ruim hein?!";
+        document.querySelector(".scorePct").style.color = "#FF0000"
+    } else if(points >= 30 && points < 70){
+        document.querySelector(".scoreText1").innerHTML = "Muito bom!";
+        document.querySelector(".scorePct").style.color = "#FFF000"
+    }else{
+        document.querySelector(".scoreText1").innerHTML = "Parabéns!";
+        document.querySelector(".scorePct").style.color = "#0D630D";
+    }
+
+
     document.querySelector(".scorePct").innerHTML = `Acertou ${points}%`;
     document.querySelector(".scoreText2").innerHTML = `Você respondeu ${questions.length} questões e acertou ${correctAnswers}`;
 
     document.querySelector(".scoreArea").style.display = "block";
     document.querySelector(".questionArea").style.display = "none";
     document.querySelector(".progress--bar").style.width = `100%`
-
-    
 }
