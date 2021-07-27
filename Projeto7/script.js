4b81d0024e071174e89b56265a4862aa043a7ee3
Victor Hugo Cunha <vhrc@cin.ufpe.br>
2021-07-26 22:27:02 -0300

    functions area

--- Projeto7/script.js
+++ Projeto7/script.js
@@ -3,6 +3,12 @@ document.querySelectorAll(".item").forEach(item => {
     item.addEventListener("dragend", dragEnd)
 });
 
+document.querySelectorAll(".area").forEach(item => {
+    item.addEventListener("dragover", dragOver);
+    item.addEventListener("dragleave", dragLeave);
+    item.addEventListener("drop", drop)
+});
+
 //functions item
 function dragStart(e){
     e.currentTarget.classList.add("dragging")
@@ -10,4 +16,14 @@ function dragStart(e){
 function dragEnd(e){
     e.currentTarget.classList.remove("dragging")
 }
+
 //functions area
+function dragOver(e){
+    e.preventDefault();
+    e.currentTarget.classList.add("hover");
+}
+function dragLeave(e){
+    e.currentTarget.classList.remove("hover");
+
+}
+function drop(){}
